5b7bd5ada8182769e993b0efa2ac401e1ef65a74
Maheswaran <maheswaran.c@cognizant.com>
2020-02-22 16:08:09 +0530

    Initial project added

---
+++ CountryDetails/CountryDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryDetails
+//
+//  Created by C, Maheswaran (Cognizant) on 22/02/20.
+//  Copyright © 2020 C, Maheswaran (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class CountryDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
